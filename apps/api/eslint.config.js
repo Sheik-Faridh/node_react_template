4658b7a1c1549cfc736dc60e5b4ab7eb806942a4
Sheik Faridh <faridh.nousadh@gmail.com>
2024-06-21 20:55:04 +0530

    refactor(App): :art: add mocha to api for unit testing

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -1,4 +1,7 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 
-export default [{ languageOptions: { globals: globals.nodeBuiltin } }, pluginJs.configs.recommended]
+export default [
+  { env: { node: true, mocha: true }, languageOptions: { globals: globals.nodeBuiltin } },
+  pluginJs.configs.recommended,
+]
